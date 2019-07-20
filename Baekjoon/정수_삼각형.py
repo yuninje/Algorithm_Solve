2d7398ec342b6b408d8e57b9c94bb03841b80d0b
INJE YUN <dbsdlswp@naver.com>
2019-07-20 11:46:50 +0900

    0720 pull origin

---
+++ Baekjoon/정수_삼각형.py
@@ -0,0 +1,25 @@
+def Max(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+N = int(input())
+
+tri = []
+for i in range(0,N):
+    tri.append(list(map(int, input().split())))
+
+dp = []
+dp.append([0])  # dp[0]
+dp.append([0,0])    # dp[1]
+
+for n in range (2, N+2): # dp[2] ~ dp[N+1]
+    dp_line = [0]
+    for i in range(1,n):
+        #dp[n][i] = Max(dp[n-1][i-1], dp[n-1][i])
+        dp_line.append(Max(dp[n-1][i-1], dp[n-1][i])+tri[n-2][i-1])
+    dp_line.append(0) 
+    dp.append(dp_line)
+
+print(max(dp[N+1]))

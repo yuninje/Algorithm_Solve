b948a59e688e375e32d66fb8391ff7f4b5e93dba
INJE YUN <dbsdlswp@naver.com>
2019-08-01 04:08:50 +0900

    modify README.md

--- Baekjoon/[ 11724 ] 연결 요소의 개수.py
+++ Baekjoon/[ 11724 ] 연결 요소의 개수.py
@@ -1,8 +1,9 @@
-# https://www.acmicpc.net/problem/11724
-
+# https://www.acmicpc.net/problem/11724   
+# PyPy3
 # N : 정점의 개수   1 <= N <= 1000
 # M : 간선의 개수   0 <= M <= N * (N-1) / 2
 # 양 끝점 u, v      1 <= u, v <= N. (u != v)
+
 import sys
 sys.setrecursionlimit(10**6)
 
@@ -10,22 +11,19 @@ def dfs(now):
     if bool_list[now]:
         return
     bool_list[now] =True
-    for a in range(0,len(arr),1):
-        if arr[a][0] == now:
-            dfs(arr[a][1])
-            del arr[a]
-            a -= 1
-        elif arr[a][1] == now:
-            dfs(arr[a][0])
-            del arr[a]
-            a -= 1
+
+    for n in range(1,N+1):
+        if arr[now][n] == 1:
+            dfs(n)
 
 N, M = list(map(int, input().split()))
-arr = []
+arr = [[0]*(N+1) for __ in range(0,N+1)]
 for m in range(0,M):
-    arr.append(list(map(int, input().split())))
+    a = list(map(int, sys.stdin.readline().split()))
+    arr[a[0]][a[1]] = 1
+    arr[a[1]][a[0]] = 1
 
-bool_list = [False for _ in range(0,N+1)]
+bool_list = [False] * (N+1)
 answer = 0
 for n in range(1,N+1):
     if not bool_list[n]:
--- Python Coding Tip.md
+++ Python Coding Tip.md
@@ -1,13 +1,3 @@
-# Algorithm Problem Solve & Algorithm Code
-
-## SWEA, 백준, 프로그래머스 문제 풀이 및 알고리즘 코드
-
-### Reference
-- **프로그래머스** : https://programmers.co.kr
-- **SWEA ( SW Expert Academy )** https://swexpertacademy.com/main/main.do
-- **백준** : https://www.acmicpc.net/
-
-
 ## Python Coding Tip
 
 ``` python
